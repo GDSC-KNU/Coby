ea0c49038b029b6ed0d3c3a333947f3b49d3228b
yun8565 <yunyun8565@naver.com>
2023-03-06 15:09:33 +0900

    feat: init project

---
+++ backend/src/test/java/com/gdsc/coby/CobyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.coby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
