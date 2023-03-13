f2c235b5ca93f351f8e9746f82ac3ccf9a995e97
yun8565 <yunyun8565@naver.com>
2023-03-13 17:04:01 +0900

    Feat: 이메일로 유저 찾기 기능 추가

--- backend/src/main/java/com/gdsc/coby/repository/UserRepository.java
+++ backend/src/main/java/com/gdsc/coby/repository/UserRepository.java
@@ -2,8 +2,12 @@ package com.gdsc.coby.repository;
 
 import com.gdsc.coby.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(@Param("email") String email);
 }
