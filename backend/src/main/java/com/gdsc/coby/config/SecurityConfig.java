295781e0987aa6b04c07b6efbb01b30553e47b4a
yun8565 <yunyun8565@naver.com>
2023-03-22 18:17:34 +0900

    Feat: Security 설정에 Swagger 경로 추가

--- backend/src/main/java/com/gdsc/coby/config/SecurityConfig.java
+++ backend/src/main/java/com/gdsc/coby/config/SecurityConfig.java
@@ -2,27 +2,19 @@ package com.gdsc.coby.config;
 
 import com.gdsc.coby.security.JwtAuthenticationFilter;
 import com.gdsc.coby.security.JwtProvider;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
-import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -59,9 +51,10 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 // 조건별로 요청 허용/제한 설정
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 // 회원가입과 로그인은 모두 승인
                 .requestMatchers("/api/signup", "/api/login").permitAll()
+                .requestMatchers("/v3/api-docs/**", "/configuration/**", "/swagger-resources/**", "/swagger-ui/**", "/webjars/**", "swagger-ui.html", "/api-docs/json").permitAll()
                 .requestMatchers("/api/users/**").hasRole("USER")
                 .anyRequest().authenticated()
                 .and()
@@ -69,25 +62,19 @@ public class SecurityConfig {
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class)
                 // 에러 핸들링
                 .exceptionHandling()
-                .accessDeniedHandler(new AccessDeniedHandler() {
-                    @Override
-                    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
-                        // 권한 문제가 발생했을 때 이 부분을 호출한다.
-                        response.setStatus(403);
-                        response.setCharacterEncoding("utf-8");
-                        response.setContentType("text/html; charset=UTF-8");
-                        response.getWriter().write("권한이 없는 사용자입니다.");
-                    }
+                .accessDeniedHandler((request, response, accessDeniedException) -> {
+                    // 권한 문제가 발생했을 때 이 부분을 호출한다.
+                    response.setStatus(403);
+                    response.setCharacterEncoding("utf-8");
+                    response.setContentType("text/html; charset=UTF-8");
+                    response.getWriter().write("권한이 없는 사용자입니다.");
                 })
-                .authenticationEntryPoint(new AuthenticationEntryPoint() {
-                    @Override
-                    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-                        // 인증문제가 발생했을 때 이 부분을 호출한다.
-                        response.setStatus(401);
-                        response.setCharacterEncoding("utf-8");
-                        response.setContentType("text/html; charset=UTF-8");
-                        response.getWriter().write("인증되지 않은 사용자입니다.");
-                    }
+                .authenticationEntryPoint((request, response, authException) -> {
+                    // 인증문제가 발생했을 때 이 부분을 호출한다.
+                    response.setStatus(401);
+                    response.setCharacterEncoding("utf-8");
+                    response.setContentType("text/html; charset=UTF-8");
+                    response.getWriter().write("인증되지 않은 사용자입니다.");
                 });
 
         return http.build();
