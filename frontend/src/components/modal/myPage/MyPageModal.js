81bcdca8d8ea34e961bcb250d56a236534868434
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-03-27 18:01:40 +0900

    Feat : 마이페이지 모달창 추가

---
+++ frontend/src/components/modal/myPage/MyPageModal.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Backdrop from "../../reuseUI/Backdrop";
+import './MyPageModal.css';
+
+
+function MyPageModal(setMyPageOpen){
+    
+    return (
+        <div>
+            <div className="modal">
+                <div className="modal-content">
+                    <div className="upper">
+                        <br />＜COBY.＞
+                    </div>
+                    <div className="userinfo">
+                        <p><br/><br/><br/>이름 : name</p>
+                        <p>그룹 : none</p>
+                        <p>등급 : grade</p>
+                        <p>기여도 : 0000</p>
+                    </div>
+                    <div className="userphoto"></div>
+                    <div className="tier"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+
+export default MyPageModal;
+
+
