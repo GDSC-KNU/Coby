a36babc4f7b9ee94a3d8d30b5097f84df1aacb87
yun8565 <yunyun8565@naver.com>
2023-03-28 22:18:48 +0900

    Feat: Swagger 설명 추가

--- backend/src/main/java/com/gdsc/coby/controller/PostController.java
+++ backend/src/main/java/com/gdsc/coby/controller/PostController.java
@@ -3,17 +3,19 @@ package com.gdsc.coby.controller;
 import com.gdsc.coby.dto.request.PostRequestDto;
 import com.gdsc.coby.dto.response.PostResponseDto;
 import com.gdsc.coby.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "게시글 컨트롤러", description = "게시글 관련 기능 제공")
 @RequiredArgsConstructor
 @RequestMapping("/api/groups")
-@Controller
+@RestController
 public class PostController {
 
     private final PostService postService;
@@ -24,6 +26,7 @@ public class PostController {
     }
 
     @GetMapping("/{groupId}/posts")
+    @Operation(description = "게시글 목록을 조회합니다.")
     public ResponseEntity<List<PostResponseDto>> getPosts(@PathVariable Long groupId) {
         return ResponseEntity.ok(postService.getPosts(groupId).stream()
                 .map(PostResponseDto::from)
@@ -31,21 +34,25 @@ public class PostController {
     }
 
     @GetMapping("/{groupId}/posts/{postId}")
+    @Operation(description = "게시글 정보를 조회합니다.")
     public ResponseEntity<PostResponseDto> getPost(@PathVariable Long postId) {
         return ResponseEntity.ok(PostResponseDto.from(postService.getPost(postId)));
     }
 
     @PostMapping("/{groupId}/posts")
+    @Operation(description = "게시글을 생성합니다.")
     public ResponseEntity<PostResponseDto> createPost(@PathVariable Long groupId, PostRequestDto requestDto) {
         return ResponseEntity.ok(PostResponseDto.from(postService.createPost(groupId, requestDto.toDto())));
     }
 
     @PutMapping("/{groupId}/posts/{postId}")
+    @Operation(description = "게시글을 수정합니다.")
     public ResponseEntity<PostResponseDto> updatePost(@PathVariable Long postId, PostRequestDto requestDto) {
         return ResponseEntity.ok(PostResponseDto.from(postService.updatePost(postId, requestDto.toDto())));
     }
 
     @DeleteMapping("/{groupId}/posts/{postId}")
+    @Operation(description = "게시글을 삭제합니다.")
     public ResponseEntity<Boolean> deletePost(@PathVariable Long postId) {
         return ResponseEntity.ok(postService.deletePost(postId));
     }
