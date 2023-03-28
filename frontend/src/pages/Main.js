9f0350c94c91c733ad36e678a5c4b41d163f6e14
KimKyuHoi <k546kh@gmail.com>
2023-03-28 21:10:01 +0900

    Design: Main 화면 수정

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,28 +1,159 @@
 import { useState } from "react";
 import Layout from "../components/reuseUI/Layout";
 import SimpleSlider from "../components/main/Carousel";
-import './Main.css'
-
-import img from '../images/menu.png'
+import "./Main.css";
+import MainImg from "../images/logo_black.png";
+import img from "../images/menu.png";
 
 function Main(props) {
-  console.log(props.cookies)
+  window.addEventListener(
+    "scroll",
+    () => {
+      document.body.style.setProperty(
+        "--scroll",
+        window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+      );
+    },
+    false
+  );
+
+  console.log(props.cookies);
   return (
-    <div className="App">
+    <div>
       <div className="Bar">
-        <Layout cookies = {props.cookies} setCookie = {props.setCookie} />
+        <Layout cookies={props.cookies} setCookie={props.setCookie} />
       </div>
       <div className="Ad">
         <SimpleSlider />
       </div>
-      <div className="introduce">
+      
+      <br />
+      <br />
+      
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="fade-in">혼자 코딩하기 힘드셨죠?</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="fade-in2">그런 여러분을 위해 찾아갑니다.</div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <div className="fade-in3">
+        <div>
+          <img src={MainImg} alt="로고" className="Main-logo" />
+        </div>
         <p className="title">실시간 개발환경 공유 플랫폼 </p>
-        <p className="word">  
-        코드 리뷰, 멘토링, 페어프로그래밍 Co (함께 개발) by.
+        <p className="word">
+          코드 리뷰, 멘토링, 페어프로그래밍 Co (함께 개발) by.
         </p>
       </div>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+
       <div className="menu">
-        <img src={img} alt='menu' className="menuimg"/>
+        <img src={img} alt="menu" className="menuimg" />
       </div>
     </div>
   );
