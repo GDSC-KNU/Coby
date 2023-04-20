a90308ce0b91785253b46200e50fab4f2caa9460
yun8565 <yunyun8565@naver.com>
2023-04-20 14:08:57 +0900

    Feat: 코드룸 조회 시 태그가 포함되도록 수정

--- backend/src/main/java/com/gdsc/coby/dto/response/RoomResponseDto.java
+++ backend/src/main/java/com/gdsc/coby/dto/response/RoomResponseDto.java
@@ -1,25 +1,33 @@
 package com.gdsc.coby.dto.response;
 
+import com.gdsc.coby.domain.constant.TagType;
 import com.gdsc.coby.dto.RoomDto;
+import com.gdsc.coby.dto.TagDto;
+
+import java.util.stream.Collectors;
 
 public record RoomResponseDto(
         Long id,
         String name,
-        Boolean password,
-        Integer personnel,
+        String language,
+        String tool,
+        String password,
         String createdBy
 ) {
 
-    public static RoomResponseDto of(Long id, String name, Boolean password, Integer personnel, String createdBy) {
-        return new RoomResponseDto(id, name, password, personnel, createdBy);
+    public static RoomResponseDto of(Long id, String name, String language, String tool, String password, String createdBy) {
+        return new RoomResponseDto(id, name, language, tool, password, createdBy);
     }
 
     public static RoomResponseDto from(RoomDto dto) {
         return new RoomResponseDto(
                 dto.id(),
                 dto.name(),
-                !dto.password().isEmpty(),
-                dto.personnel(),
+                dto.tags().stream().filter(tag -> tag.tagType().equals(TagType.LANGUAGE))
+                        .map(TagDto::name).collect(Collectors.joining(",")),
+                dto.tags().stream().filter(tag -> tag.tagType().equals(TagType.TOOL))
+                        .map(TagDto::name).collect(Collectors.joining(",")),
+                dto.password(),
                 dto.createdBy()
         );
     }
