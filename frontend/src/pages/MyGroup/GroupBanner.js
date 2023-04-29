6fb78d0e3e7c33157f41f69f07573eaa71145a54
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-04-30 04:48:30 +0900

    Feat : GroupBanner 구현

--- frontend/src/pages/MyGroup/GroupBanner.js
+++ frontend/src/pages/MyGroup/GroupBanner.js
@@ -1,5 +1,7 @@
 import "./GroupBanner.css"
 
+import groupimg from '../../images/GroupImg.png'
+
 const GroupBanner = () => {
     function NoGroupHandleClick(event){
         window.location.href="/Nogroup";
@@ -19,10 +21,35 @@ const GroupBanner = () => {
 
     return (
         <div>
-            <button onClick={NoGroupHandleClick}>그룹x</button>
-            <button onClick={GroupInfoHandleClick}>그룹정보</button>
-            <button onClick={GroupBoardHandleClick}>그룹게시판</button>
-            <button onClick={GroupMemberHandleClick}>그룹원정보</button>
+            <div className="groupbanner_body">
+                
+                <div className = "group_imgbox">
+                    <img src = {groupimg}/>
+                </div>
+                
+                <div className = "group_name">
+                    (그룹 이름)
+                </div>
+
+                {/*------------------------------------*/}
+
+                {/* <div>
+                    <button className = "groupbtn" onClick={NoGroupHandleClick}>그룹x</button>
+                </div>  이부분은 그룹이 없을때만 나타나도록.*/}
+                
+                <div>
+                    <button className = "groupbtn" onClick={GroupInfoHandleClick}>그룹정보</button>
+                </div>
+                
+                <div>
+                    <button className = "groupbtn" onClick={GroupBoardHandleClick}>그룹게시판</button>
+                </div>
+                
+                <div>
+                    <button className = "groupbtn" onClick={GroupMemberHandleClick}>그룹원목록</button>
+                </div>
+
+            </div>
         </div>
     );
 }
