f6556aad04f3f33a42836f76c5f6464ebf5ec371
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-05-20 20:37:01 +0900

    Fix : NoGroup CSS 수정

--- frontend/src/pages/MyGroup/NoGroup.js
+++ frontend/src/pages/MyGroup/NoGroup.js
@@ -1,6 +1,6 @@
 import Layout from "../../components/reuseUI/Layout";
 import { useState } from "react";
-import "./NoGroup.css"
+import styles from "./NoGroup.module.css"
 import logo from '../../images/NoGroup.png'
 import Backdrop from "../../components/reuseUI/Backdrop";
 import MyGroupMake from "../../components/modal/makeGroup/makeGroup";
@@ -18,13 +18,13 @@ const Nogroup = () => {
   return (
     <div className="outer">
         <Layout />
-        <div className="nogroup_main">
-            <p className="nogroup_font">가입된 그룹이 없습니다.</p>
-            <img src={logo} alt="그룹" className="nogroup-image" />
+        <div className={styles.sogroup_main}>
+            <p className={styles.nogroup_font}>가입된 그룹이 없습니다.</p>
+            <img src={logo} alt="그룹" className= {styles.nogroup_image} />
         </div>
         <div className="sub">
-            <button className="search_btn1">+그룹 탐색하기</button>
-            <button className="search_btn1" onClick={showGroupMakeModal}>+그룹 생성하기</button>
+            <button className={styles.search_btn1}>+그룹 탐색하기</button>
+            <button className={styles.search_btn1} onClick={showGroupMakeModal}>+그룹 생성하기</button>
         </div>
           {groupMakeOpen && <MyGroupMake setModalOpen={setGroupMakeModalOpen}/>}
           {groupMakeOpen && <Backdrop onCancel={closeGroupMakeModal} />}
