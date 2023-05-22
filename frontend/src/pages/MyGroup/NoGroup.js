d520c209d7524af384af7744afdfd2aaf3f30132
Seung Woon <113246980+Usimth@users.noreply.github.com>
2023-05-22 10:34:56 +0900

    Fix : No Group CSS 추가수정

--- frontend/src/pages/MyGroup/NoGroup.js
+++ frontend/src/pages/MyGroup/NoGroup.js
@@ -16,13 +16,13 @@ const Nogroup = () => {
   }
 
   return (
-    <div className="outer">
+    <div className={styles.outer}>
         <Layout />
         <div className={styles.sogroup_main}>
             <p className={styles.nogroup_font}>가입된 그룹이 없습니다.</p>
             <img src={logo} alt="그룹" className= {styles.nogroup_image} />
         </div>
-        <div className="sub">
+        <div className={styles.sub}>
             <button className={styles.search_btn1}>+그룹 탐색하기</button>
             <button className={styles.search_btn1} onClick={showGroupMakeModal}>+그룹 생성하기</button>
         </div>
