f88f4ba79f1562547528872b6a12d7c7e4a7da4e
sunghoon <102848645+20220402@users.noreply.github.com>
2023-08-30 14:40:15 +0900

    feat: 오타수정

--- frontend/src/components/pages/DetailPage/CodeRoom/CodeReview/components/NarrowSidebar.js
+++ frontend/src/components/pages/DetailPage/CodeRoom/CodeReview/components/NarrowSidebar.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../../../../../../assets/NarrowGroup";
+import logo from "../../../../../../assets/NarrowGroup.png";
 
 import styles from "./css/Sidebar.module.css";
 import exit from "../../../../../../assets/exit.png";
